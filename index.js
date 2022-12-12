e45c0427c8b3821bf70054bc7c4dc10eb0d4522a
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-12-12 21:08:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
 function hasTargetSum(array, target) {
-  // for element i in array
+  
   for(let i = 0; i < array.length-1; i++){
-    //   for each other element j in the array
+    
     for(let j = i+1; j < array.length; j++){
-          //     If their sum matches the target sum
+          
       if(array[i]+array[j] === target){
-        // return true
+        
         return true
       }
     }
   } 
-  // If none of the sums match the target sum
-  //   return false
+ 
   return false;
 }
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-  iterate over the array of numbers
-  for the current number, identify a comnplementary mumber that adds to our target
-  example is if our number is 2 and target is 5, the completary number is 3
-  iterate over the remaining numbers in the array
-    check if any of the remaining number is the complement
-       if so, return true
-  if we reach the end of the array, return false
 
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
